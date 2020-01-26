0938d2cf9a7fc6aa2c0e366e83408e01761c0035
Alfredo Ryelcius <42674799+aryelciu001@users.noreply.github.com>
2020-01-26 22:23:17 +0800

    Set up CORS Headers

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+//express setup
 const ex = require("express");
 const app = ex();
+
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 //spawnSync is used to spawn a child process synchronously, meaning that it stops everything until it finishes
 //spawn is like spawnSync but asynchronous
@@ -7,6 +32,14 @@ const { spawn, spawnSync } = require("child_process");
 
 watchedStocks = [];
 
+//Initial data fetch. Synchronous data fetch
+let { stdout, stderr } = spawnSync("python", [
+  "./python/test.py",
+  "pythonProgram"
+]);
+watchedStocks = stdout.toString();
+
+//next data fetch. Asynchronous data fetch
 const update = () => {
   console.log("update");
   pyProgram = spawn("python", ["./python/test.py", "pythonProgram"]);
@@ -19,12 +52,7 @@ const update = () => {
   });
 };
 
-let { stdout, stderr } = spawnSync("python", [
-  "./python/test.py",
-  "pythonProgram"
-]);
-watchedStocks = stdout.toString();
-
+//Update every 10 seconds. Long Polling
 setInterval(() => {
   update();
 }, 10000);
@@ -38,12 +66,8 @@ app.get("/python", (req, res) => {
   res.send(watchedStocks);
 });
 
+//Server
 const port = process.env.PORT || 8000;
-
 app.listen(port, () => {
   console.log(`app listening to port ${port}`);
 });
-
-//   exec("python python/test.py", (err, stdout, stderr) => {
-//     console.log("python out here ", stdout);
-//   });
